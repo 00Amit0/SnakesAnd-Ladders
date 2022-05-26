7840680f2157e55576032a8ccb53efa50aa5270f
“00Amit0” <amit836961@gmail.com>
2022-05-26 21:32:00 +0530

    Welcome To Snakes and Ladders Game

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SnakesAndLadders
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Let's play snake and Ladders!");
+
+            //Dice dc = new Dice();
+            //dc.game();
+
+            Snake_Ladder sk = new Snake_Ladder();
+            sk.game();
+        }
+    }
+}
